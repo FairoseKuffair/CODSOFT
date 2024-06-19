3e1b06e8ff88b91f43445af7da6b15a2a4c88ca8
FairoseKuffair <skfairose10@gmail.com>
2024-06-19 23:55:58 +0530

    Add files via upload

---
+++ RockPaperScissor.py
@@ -0,0 +1,19 @@
+import random
+
+def play():
+    user = input("What's your choice? 'R' for rock, 'P' for paper, 'S' for scissors\n")
+    computer = random.choice(['R', 'P', 'S'])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+def is_win(player, opponent):
+    if (player == 'R' and opponent == 'S') or (player == 'S' and opponent == 'P') or (player == 'P' and opponent == 'R'):
+        return True
+
+print(play())
